dbbdf08291f934711f4969b560cbeb0013b9aa33
acelest <eyengadjou@gmail.com>
2024-12-24 01:34:53 +0530

    Add default layout with Header and Hero components

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Header />
+    <Hero />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+import Hero from "@/components/Hero.vue";
+import Header from "@/components/Header.vue";
+</script>
